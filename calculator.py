baf6ee93967bf76571d750ee12442552a21e3e37
Liz <lizwkariuki58@gmail.com>
2021-09-04 15:36:15 +0300

    add text

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ st.set_page_config(
 #header 1
 st.title("MMF Compound Interest Calculator")
 
+st.write("Visit my site @ https://lizkariuki.com/.Code hosted on GitHub:https://github.com/lizwkariuki58/MMF-Calculator.")
+
 st.header("**Investing a Lumpsum**")
 #define columns
 Principalcolumn, Interestcolumn, Frequencycolumn, Timecolumn = st.columns(4)
